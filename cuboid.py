1ab9cc64372b347b9b8cd6ea8e790c10c26d3438
Remya Anandaraj <remyaanandaraj2005@gmail.com>
2025-10-30 01:33:21 -0800

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,4 @@
+def surface_area(length,breadth,height):
+           return 2*(length*breadth+breadth*height+height*length)
+def perimeter(length,breadth,height):
+           return 4*(length+breadth+height)
