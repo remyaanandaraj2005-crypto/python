7e0f368db9be48e92eb2c18f4a3747fc986b2ef7
Remya Anandaraj <remyaanandaraj2005@gmail.com>
2025-10-30 23:10:47 -0800

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,9 @@
+from math1 import arithmetic,rectangle
+from math1.advance import triangle
+
+print("Addition of two numbers:",arithmetic.add_numbers(10,5))
+print("Subtraction of two numbers:",arithmetic.subtract_numbers(10,5))
+
+print("Area of rectangle:",rectangle.area(10,5))
+print("Area of triangle:",triangle.triangle_area(10,5))
+
