d11f1b484c653b5fcd1d607c3cf93c1c6c5c520f
Remya Anandaraj <remyaanandaraj2005@gmail.com>
2025-11-06 14:53:00 +0530

    Add files via upload

---
+++ co3_A.py
@@ -0,0 +1,7 @@
+limit=int(input("Enter the limit:"))
+list=[]
+for i in range(limit):
+          num=int(input("Enter the limit:"))
+          if(num>0):                                  
+               list.append(num)
+print(list)
